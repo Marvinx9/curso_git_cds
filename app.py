28d9dc68bd699ea719549e7f6b0d8236bef2a676
Afranio Dantas <afraniodantas0224@gmail.com>
2023-12-14 17:36:49 -0300

    atualization tema streamlit and title

--- app.py
+++ app.py
@@ -1,21 +1,34 @@
 import pandas as pd
 import streamlit as st
+from PIL import Image
 
 import src.answers as asw
 from src.extraction import load_data
 
-st.set_page_config(layout="wide")
+st.set_page_config(page_title='Início',
+    page_icon='📋',layout="wide"
+    )
 
+st.sidebar.markdown('# Xtreme Groovi Bikes Sales')
+st.sidebar.markdown("""---""")
+image = Image.open( 'logo.png' )
+st.sidebar.image( image, width=270)
+
+st.sidebar.markdown('### Revenda de bicicletas')
+st.sidebar.markdown("""---""")
+st.markdown('## Sobre o projeto')
+st.write("#### Diante do aumento constante do valor dos veículos usados, a renomada XGB Sales está em busca de ampliar sua atuação no mercado. Nosso objetivo é facilitar a busca por excelentes oportunidades de negócio, especialmente no segmento de motocicletas para revenda. Comprometidos com a qualidade e satisfação dos nossos clientes, buscamos continuamente as melhores opções disponíveis no mercado. Ressaltamos que todo o contexto, personagens e perguntas apresentados são totalmente fictícios.")
 
 def create_dataframe_section(df):
-    st.title("Sctions - Database Description")
-
+    
+    st.markdown("## - Análise dos Dados")
+    
     col_1, col_2 = st.columns(2)
 
-    col_1.header("Database")
+    col_1.header("Base de Dados")
     col_1.dataframe(df, height=530)
 
-    col_2.header("Data Description")
+    col_2.header("Descrições")
 
     data_description = """
                         | Coluna | Descrição |
@@ -39,44 +52,44 @@ def create_dataframe_section(df):
 
 
 def create_answers_section(df):
-    st.title("Main Questions Answers")
+    st.title("Principais Análises")
 
-    st.header("First Round")
     st.subheader(
-        "How many bikes are being sold by their owners and how many bikes are being sold by distributors?"
+        "EmpresaS que tem bicicletas mais caras cadastradas e as que possuem maior quantidade de veículos cadastrados"
     )
+    asw.rd3_question_5(df)
+
+    st.subheader(
+        "Quantas bicicletas estão sendo vendidas pelos seus proprietários e quantas bicicletas estão sendo vendidas pelos distribuidores")
     asw.rd1_question_9(df)
 
-    st.subheader("How many bikes are being sold are bikes from a unique owner?")
+    st.subheader("Quantas bicicletas estão sendo vendidas são de um único proprietário?")
     asw.rd1_question_13(df)
 
     st.subheader(
-        "Are high kilometer bikes more expensive than bikes with lower kilometer?"
+        "As bicicletas de alta quilometragem são mais caras do que as bicicletas de menor quilometragem?"
     )
     asw.rd1_question_14(df)
 
     st.subheader(
-        "Are the bikes with a unique owner more expense on avarege than the other bikes?"
+        "As bicicletas com um proprietário único são mais caras do que as outras bicicletas?"
     )
     asw.rd2_question_1(df)
 
     st.subheader(
-        "Are the bikes that have more owners also the bikes with more kilometers traveled on avarege?"
+        "As bicicletas que têm mais proprietários são também as bicicletas com mais quilómetros percorridos em média?"
     )
     asw.rd2_question_2(df)
 
-    st.subheader("Which company has the most bikes registered?")
+    st.subheader("Qual empresa tem mais bicicletas cadastradas?")
     asw.rd2_question_7(df)
 
-    st.subheader("Which company has the most expensive bikes on avarege?")
+    st.subheader("Qual empresa tem as bicicletas mais caras do mercado?")
     asw.rd3_question_2(df)
 
-    st.subheader(
-        "Are the company that has the most expensive bikes registered also the company with the most bikes registered?"
-    )
-    asw.rd3_question_5(df)
+    
 
-    st.subheader("Which bikes are good for buying?")
+    st.subheader("Quais bicicletas são boas para comprar?")
     asw.rd3_question_7(df)
 
 
